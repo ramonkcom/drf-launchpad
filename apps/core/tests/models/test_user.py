43abcda7496b5b77d076cb883dea3e70c475890b
Ramon K <contato@ramonk.com>
2023-06-05 16:13:52 -0300

    refactor(tests): separate managers tests

--- apps/core/tests/models/test_user.py
+++ apps/core/tests/models/test_user.py
@@ -22,19 +22,6 @@ class UserModelTests(UserTestsMixin,
                                 username='t3s7')
         self.assertEqual(str(user), 't3s7 (test@test.com)')
 
-    def test_manager_create_user_creates_username(self):
-        """`UserManager.create_user` creates username when not provided
-        """
-
-        self.assertEqual(User.objects.count(), 0)
-        user_1 = self.create_user(email='test@example1.com')
-        self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(user_1.username, 'test')
-
-        user_2 = self.create_user(email='test@example2.com')
-        auto_username = 'test_' + str(int(datetime.now().timestamp()))[-5:-2]
-        self.assertTrue(user_2.username.startswith(auto_username))
-
     def test_creating_user_creates_person(self):
         """Creating an `User` also creates a `Person`
         """
@@ -55,22 +42,3 @@ class UserModelTests(UserTestsMixin,
 
         user.delete()
         self.assertEqual(Person.objects.count(), 0)
-
-    def test_manager_create_user_accepts_person_data(self):
-        """`UserManager.create_user` accepts personal data
-        """
-
-        self.assertEqual(User.objects.count(), 0)
-        user = User.objects.create_user(
-            email='valid.email@test.com',
-            password='valid#password@123',
-            person={
-                'given_name': 'Ramon',
-                'family_name': 'Kayo',
-            }
-        )
-        self.assertEqual(Person.objects.count(), 1)
-        self.assertEqual(Person.objects.first(), user.person)
-
-        self.assertEqual(user.person.given_name, 'Ramon')
-        self.assertEqual(user.person.family_name, 'Kayo')
