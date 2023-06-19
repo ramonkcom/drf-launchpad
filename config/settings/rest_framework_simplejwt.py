2dafab1b0942f09f64c81c2b367cadcda88098f6
Ramon K <contato@ramonk.com>
2023-06-19 15:01:40 -0300

    feat!: change header type from 'Bearer' by 'JWT'

--- config/settings/rest_framework_simplejwt.py
+++ config/settings/rest_framework_simplejwt.py
@@ -13,7 +13,7 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'AUTH_HEADER_TYPES': ['Bearer',],
+    'AUTH_HEADER_TYPES': ['JWT',],
     'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
     'ROTATE_REFRESH_TOKENS': True,
     'USER_AUTHENTICATION_RULE': 'apps.core.utils.auth.user_authentication_rule',
