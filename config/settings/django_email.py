d52ab193f8326548b8e7168f4e633020b092c203
Ramon K <contato@ramonk.com>
2023-06-28 17:31:32 -0300

    feat(config.settings.django_email): add settings

--- config/settings/django_email.py
+++ config/settings/django_email.py
@@ -8,7 +8,43 @@ Full list of Django settings:
 - https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
+import os
+
+from dotenv import load_dotenv
+
 from .common import PROJECT_TITLE
+
+load_dotenv()
+
+# ---------------------------------------------------------------------------- #
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-host
+
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'localhost')
+
+# ---------------------------------------------------------------------------- #
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-port
+
+EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
+
+# ---------------------------------------------------------------------------- #
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-host-user
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+
+# ---------------------------------------------------------------------------- #
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-host-password
+
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+
+# ---------------------------------------------------------------------------- #
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-use-tls
+
+EMAIL_USE_TLS = False
+
+# ---------------------------------------------------------------------------- #
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-use-ssl
+
+EMAIL_USE_SSL = True
 
 # ---------------------------------------------------------------------------- #
 # NOTE: This is a custom setting for the email confirmation functionality.
